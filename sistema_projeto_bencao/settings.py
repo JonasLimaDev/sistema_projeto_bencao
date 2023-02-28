ddf86f8e7390d5a258606e5b6eaa4aaae627ae90
Jonas Lima <jonas.lima.dev@gmail.com>
2023-02-28 08:35:13 -0300

    adicionado arquivos bootstrap estáticos

--- sistema_projeto_bencao/settings.py
+++ sistema_projeto_bencao/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+    f'{BASE_DIR}/files/static_base/',
+]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'files/static_files')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
